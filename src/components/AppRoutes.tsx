c13365c37f6d4ab08e43b690ad43d53a1ab0ad9f
SmolKate <zdobnovaea@gmail.com>
2025-06-15 18:00:53 +0300

    feat: add routes for login

---
+++ src/components/AppRoutes.tsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from 'react-router-dom'
+// import Home from '../pages/Home'
+// import CategoriesLayout from '../layout/CategoriesLayout'
+import { appPaths } from '../utils/appPaths'
+import { Home } from '../pages/Home'
+import { Login } from '../pages/Login'
+import { Registration } from '../pages/Registration'
+import { NotFound } from '../pages/NotFound'
+// import PrivateRoute from './PrivateRoute'
+
+const AppRoutes = () => {
+    return (
+        <>
+            <Routes>
+                <Route path={appPaths.home} element={<Home />} />
+                <Route path={appPaths.login} element={<Login />} />
+                <Route path={appPaths.registration} element={<Registration />} />
+                <Route path="*" element={<NotFound />} />
+
+                {/* <Route path={appPaths.contentRoot} element={<CategoriesLayout />}>
+                    <Route path={appPaths.category(':name')} element={<PrivateRoute><Category /></PrivateRoute>} />
+                    <Route path={appPaths.character} element={<PrivateRoute><Character /></PrivateRoute>} />
+                    <Route path={appPaths.location} element={<PrivateRoute><Place /></PrivateRoute>} />
+                    <Route path={appPaths.episode} element={<PrivateRoute><Episode /></PrivateRoute>} />
+                    <Route path="*" element={<NotFound />} />
+                </Route> */}
+            </Routes>
+        </>
+    )
+}
+
+export default AppRoutes
---
+++ src/utils/appPaths.ts
@@ -0,0 +1,5 @@
+export const appPaths = {
+    home: '/',
+    login: '/login',
+    registration: '/registration',
+}
